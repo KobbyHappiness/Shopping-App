ddc3cff80addd83aab3943abf70da6d51b7b0158
KobbyHappiness <philipcudjoe207@gmail.com>
2021-11-17 21:45:41 +0000

    navigate to cart page on single product page

--- src/screens/product.js
+++ src/screens/product.js
@@ -1,68 +1,85 @@
-import React from "react";
-import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { Image, ScrollView, StyleSheet, Text, TouchableWithoutFeedback, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import {Feather} from '@expo/vector-icons';
 import consts from "../constansts";
 import RoundButton from "../components/roundButton";
 import constansts from "../constansts";
-import image from '../../assets/hoodie2.png';
+import { useCart } from "../hooks/cart";
 
-const ProductScreen = ({navigation})=>{
+const ProductScreen = ({navigation, route: {params: {item}}})=>{
+    const [color, setColor] = useState(item.images[0].color);
+    const [image, setImage] = useState(item.images[0].source);
+    const [size, setSize] = useState(item.sizes[0]);
+    const {addToCart} = useCart();
+
+    const handleSelectColor = (feedback)=>{
+        setColor(feedback.color);
+        setImage(feedback.source);
+    }
+
+    const handleGoToCart = ()=>{
+        addToCart({
+            name: item.name, 
+            price: item.price,
+            size, image, color,
+        });
+        navigation.navigate("Cart");
+    }
+
     return <View style={{flex: 1}}>
         <SafeAreaView edges={["top"]} />
          <View style={styles.homeHeader}>
              <RoundButton onPress={navigation.goBack} content={<Feather name="chevron-left" color="#fff" size={22} />} />
-            <RoundButton light={true} content={<Feather name="shopping-bag" color="#000" size={22} />} />
+            <RoundButton onPress={()=>navigation.navigate("Cart")} light={true} content={<Feather name="shopping-bag" color="#000" size={22} />} />
          </View>
          <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
              <Image source={image} style={{width: "80%", height: "100%", resizeMode: "contain"}} />
          </View>
          <View style={{flex: 1.5, borderTopEndRadius: 30, borderTopStartRadius: 30, backgroundColor: "#fff"}}>
             <ScrollView showsVerticalScrollIndicator={false} overScrollMode={"never"} style={{flex: 1, paddingHorizontal: consts.spaceX, paddingVertical: 20}}>
+                {/* Name And Price section */}
                 <View style={{flexDirection: "row", justifyContent: "space-between", marginBottom: 30}}>
-                    <Text style={{fontFamily: consts.mediumFont, fontSize: 20}}>Winter Hoodie</Text>
-                    <Text style={{fontFamily: consts.boldFont, fontSize: 22}}>&cent; 240.00</Text>
+                    <Text style={{fontFamily: consts.mediumFont, fontSize: 20}}>{item.name}</Text>
+                    <Text style={{fontFamily: consts.boldFont, fontSize: 22}}>&cent; {item.price.toFixed(2)}</Text>
                 </View>
                 <View style={{flexDirection: "row", marginBottom: 30}}>
+
+                    {/* Sizes Section */}
                     <View style={styles.sizeBox}>
                         <Text style={{fontFamily: consts.regularFont, fontSize: 17}}>Choose Size</Text>
                         <View style={{flexDirection: "row", paddingVertical: 20}}>
-                            <View style={styles.sizeButton}>
-                                <Text style={{fontSize: 25, fontFamily: consts.regularFont}}>S</Text>
-                            </View>
-                            <View style={styles.sizeButton}>
-                                <Text style={{fontSize: 25, fontFamily: consts.regularFont}}>M</Text>
-                            </View>
-                            <View style={styles.sizeButton}>
-                                <Text style={{fontSize: 25, fontFamily: consts.regularFont}}>L</Text>
-                            </View>
-                            <View style={styles.sizeButton}>
-                                <Text style={{fontSize: 25, fontFamily: consts.regularFont}}>XL</Text>
-                            </View>
+                            {item.sizes.map(s=> <TouchableWithoutFeedback onPress={()=> setSize(s)}>
+                                <View key={s} style={[styles.sizeButton, {borderColor: s === size ? "black" : "#eee"}]}>
+                                    <Text style={{fontSize: 25, fontFamily: consts.regularFont}}>{s}</Text>
+                                </View>
+                            </TouchableWithoutFeedback>)}
                         </View>
                     </View>
+
+                    {/* Colors Section */}
                     <View style={styles.colorsBox}>
-                        <View style={{...styles.colorSelectIndicator, borderColor: "black"}}>
-                            <View style={{...styles.colorItem, backgroundColor: "black"}} />
-                        </View>
-                        <View style={{...styles.colorSelectIndicator, borderColor: "transparent"}}>
-                            <View style={{...styles.colorItem, backgroundColor: "grey"}} />
-                        </View>
-                        <View style={{...styles.colorSelectIndicator, borderColor: "transparent"}}>
-                            <View style={{...styles.colorItem, backgroundColor: "green"}} />
-                        </View>
+                        {item.images.map(img=> <TouchableWithoutFeedback onPress={()=> handleSelectColor(img)}>
+                            <View style={{...styles.colorSelectIndicator, borderColor: color === img.color ? "black" : "transparent"}}>
+                                <View style={{...styles.colorItem, backgroundColor: img.color }} />
+                            </View>
+                        </TouchableWithoutFeedback>)}
                     </View>
                 </View>
+
+                {/* Description Section */}
                 <View>
                     <Text style={{fontFamily: consts.regularFont, fontSize: 17, marginBottom: 10}}>Description</Text>
                     <Text style={{fontSize: 16, fontFamily: consts.regularFont, color: "#777", lineHeight: 30}}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi excepturi voluptate fugit eligendi quae quaerat doloribus delectus illo similique! Sint placeat, voluptas nihil eos deserunt autem ut omnis quibusdam temporibus. Incidunt accusamus esse doloremque labore praesentium, fuga eaque sit mollitia?</Text>
                 </View>
             </ScrollView>
             <View style={{paddingHorizontal: constansts.spaceX, paddingVertical: 10}}>
-                <View style={{backgroundColor: "orange", padding: 20, borderRadius: 15, justifyContent: "center", flexDirection: "row"}}>
-                    <Feather name="shopping-bag" size={22} color="#fff" style={{marginRight: 10}} />
-                    <Text style={{color: "#fff", fontFamily: consts.mediumFont, fontSize: 16}}>Add to Cart</Text>
-                </View>
+                <TouchableWithoutFeedback onPress={handleGoToCart}>
+                    <View style={{backgroundColor: "orange", padding: 20, borderRadius: 15, justifyContent: "center", flexDirection: "row"}}>
+                        <Feather name="shopping-bag" size={22} color="#fff" style={{marginRight: 10}} />
+                        <Text style={{color: "#fff", fontFamily: consts.mediumFont, fontSize: 16}}>Add to Cart</Text>
+                    </View>
+                </TouchableWithoutFeedback>
                 <SafeAreaView edges={["bottom"]} />
             </View>
          </View>
