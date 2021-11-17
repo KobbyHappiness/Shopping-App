987cddb4c02f3d5ce205ccacd2980bfd09d8889f
KobbyHappiness <philipcudjoe207@gmail.com>
2021-11-17 21:40:09 +0000

    cart provdier for cart page

--- App.js
+++ App.js
@@ -5,6 +5,10 @@ import Home from './src/screens/home';
 import { useFonts, Poppins_400Regular, Poppins_600SemiBold, Poppins_800ExtraBold } from '@expo-google-fonts/poppins';
 import { View } from 'react-native';
 import ProductScreen from './src/screens/product';
+import CartScreen from './src/screens/cart';
+import CartProvider from './src/hooks/cart';
+import LoginScreen from './src/screens/login';
+import SignupScreen from './src/screens/signup';
 
 const Stack = createNativeStackNavigator();
 
@@ -15,11 +19,16 @@ export default function App() {
 
   if (!fontsLoaded) return <View />;
   else return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Product" component={ProductScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <CartProvider>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName="Home">
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Product" component={ProductScreen} />
+          <Stack.Screen name="Cart" component={CartScreen} />
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Signup" component={SignupScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </CartProvider>
   );
 }
